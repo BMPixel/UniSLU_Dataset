1f3c80b9dc6eaaad05b3ca26a7e7a9890a19bdaa
BMPixel <pixelwenbo@gmail.com>
2022-12-13 21:49:04 +0800

    build processed datasets

---
+++ utils/data_preprocess.py
@@ -0,0 +1,256 @@
+""" 
+Loaded datasets examples
+{
+    "train": {
+        "dialogues": [
+            {
+                "dialogue_id": 0,
+                "turns": [{
+                    "turn_id": 0,
+                    "speaker": "user",
+                    "utterance": "Hello. Please play the music named Whisper.",
+                    "annotations": 
+                        {
+                            "intent": "MUSIC",
+                            "slots": {
+                                "SONG": "Whisper"
+                            }
+                        }
+                }]
+            }, 
+            {
+                "dialogue_id": 1, 
+                ...
+            }
+        ],
+        "meta": { ... }
+    },
+    "test": { ... },
+    "dev": { ... }
+}
+"""
+# %% Import modules
+
+
+# %% Define atis dataset functions
+def _read_one_atis_sample(obj, f, dataset_name, intent2slots):
+    """Read one sample of ATIS datasets
+
+    :param obj: dataset object
+    :param f: file handle
+    :param dataset_name: dataset name, dev, test or train
+    :return: whether successfully read one sample
+    """
+    sentence = []
+    taggings = []
+    line = f.readline().strip()
+    if line == '':
+        return False
+    while line != '':
+        pair = line.split(' ')
+        sentence.append(pair[0])
+        if len(pair) > 1:
+            taggings.append(pair[1])
+        line = f.readline().strip()
+    sentence, intent = sentence[:-1], sentence[-1]
+    # update intent-slot mapping
+    if intent not in intent2slots:
+        intent2slots[intent] = set()
+
+    slots = {}
+    for i, tag in enumerate(taggings):
+        if tag.startswith('B-'):
+            slots[tag[2:]] = sentence[i]
+            intent2slots[intent].add(tag[2:])
+        if tag.startswith('I-'):
+            slots[tag[2:]] += " " + sentence[i]
+
+    obj[dataset_name]['dialogues'].append({
+        "dialogue_id": len(obj[dataset_name]['dialogues']),
+        "turns": [{
+            "turn_id": 0,
+            "speaker": "user",
+            "utterance": " ".join(sentence).strip(),
+            "annotations": {
+                "intent": intent,
+                "slots": slots
+            }
+        }]
+    })
+    return True
+
+
+def load_atis_dataset(path='raw_datasets/ATIS'):
+    """Load ATIS dataset from given dictionary
+
+    :param path: data path, defaults to 'raw_datasets/ATIS'
+    :type path: str, optional
+    :return: dataset obj
+    :rtype: dict
+    """
+    obj = {}
+    for dataset_name in ['dev', 'test', 'train']:
+        with open(os.path.join(path, dataset_name+'.txt'), 'r') as f:
+            while True:
+                if not _read_one_atis_sample(obj, f, dataset_name, intent2slots):
+                    break
+
+    # turn intent2slots into list dict
+    for intent in intent2slots:
+        intent2slots[intent] = list(intent2slots[intent])
+    for dataset_name in ['dev', 'test', 'train']:
+        obj[dataset_name]['meta']['intent2slots'] = intent2slots
+
+    return obj
+
+
+# %% Define HWU64 dataset loading functions
+def load_hwu64_dataset(path='raw_datasets/HWU64', partition=[0.8, 0.1, 0.1]):
+    """Load a HWU64 dataset
+
+    :param path: dataset path, defaults to 'raw_datasets/HWU64'
+    :type path: str, optional
+    :param partition: partition ratio of train/dev/test set, defaults to [0.8, 0.1, 0.1]
+    :type partition: list, optional
+    """
+    train_size, validate_size, test_size = partition
+    intent2slots = {}
+    df = pd.read_csv(os.path.join(path, 'all.txt'), sep=';').reset_index()
+
+    samples = []
+    for row in df.iloc:
+        sample = transform_one_hwu64_sample(row, intent2slots)
+        if sample != None:
+            samples.append(sample)
+
+    np.random.seed(42)
+    np.random.shuffle(samples)
+    train, dev, test = np.split(
+        samples, [int(train_size * len(samples)), int(1 - test_size) * len(samples)])
+
+    # turn intent2slots into list dict
+    for intent in intent2slots:
+        intent2slots[intent] = list(intent2slots[intent])
+
+    return {
+        'train': {
+            'dialogues': train,
+            'meta': {'intent2slots': intent2slots}
+        },
+        'dev': {
+            'dialogues': dev,
+            'meta': {'intent2slots': intent2slots}
+        },
+        'test': {
+            'dialogues': test,
+            'meta': {'intent2slots': intent2slots}
+        }
+    }
+
+
+def transform_one_hwu64_sample(row: pd.DataFrame, intent2slots):
+    """turn a dataframe form sample into a object one and update intent2slots
+
+    :param row: one sample
+    :type row: pd.DataFrame
+    :param intent2slots: intent2slots reference
+    :type intent2slots: Any
+    :return: object sample
+    """
+    if type(row['answer_annotation']) != str:
+        return None
+
+    intent = row['scenario'] + '_' + row['intent']
+    if intent not in intent2slots:
+        intent2slots[intent] = set()
+    # extract entity
+    reg = re.compile(r'\[[^\[\]]*\]')
+    slots = {}
+    slot_pattern = reg.findall(row['answer_annotation'])
+    utterance = row['answer_annotation']
+    for pattern in slot_pattern:
+        # print(pattern)
+        name, value = pattern[1:-1].split(':')
+        slots[name.strip()] = value.strip()
+        intent2slots[intent].add(name.strip())
+        utterance = utterance.replace(pattern, value.strip())
+
+    return {
+        "dialogue_id": int(row['index']),
+        "turns": [{
+            "turn_id": 0,
+            "speaker": "user",
+            "utterance": utterance.strip(),
+            "annotations": {
+                "intent": intent,
+                "slots": slots
+            }
+        }]
+    }
+
+
+# %% Define TOPv2 dataset loading functions
+def load_top_datset(path='raw_datasets/TOPv2'):
+    """Load a TopV2 dataset
+
+    :param path: dataset path, defaults to 'raw_datasets/TOPv2'
+    :type path: str, optional
+    :return: dataset object
+    """
+    obj = {}
+    domains = ['alarm', 'event', 'messaging', 'music',
+               'navigation', 'reminder', 'timer', 'weather']
+    intents = set()
+    for dataset_name in ['train', 'dev', 'test']:
+        if dataset_name == 'train':
+            dataset_name_suffix = '_train.tsv'
+        elif dataset_name == 'dev':
+            dataset_name_suffix = '_eval.tsv'
+        else:
+            dataset_name_suffix = '_test.tsv'
+        obj[dataset_name] = {
+            'dialogues': [],
+            'meta': {}
+        }
+
+        for domain in domains:
+            domain_dialogues, domain_intents = transform_one_top_dataset_file(
+                os.path.join(path, domain+dataset_name_suffix))
+            obj[dataset_name]['dialogues'] += domain_dialogues
+            intents.update(domain_intents)
+
+    for dataset_name in ['train', 'dev', 'test']:
+        obj[dataset_name]['meta']['intent2slots'] = intents
+
+    return obj
+
+
+def transform_one_top_dataset_file(path):
+    """Turn one top dataset file in data array and an intent set
+
+    :param path: file path
+    :return: data list and sets of all intents
+    """
+    print('Loading dataset', path)
+    dialogues = []
+    intents = set()
+    df = pd.read_csv(path, sep='\t').reset_index()
+    for row in df.iloc:
+        reg = re.compile('IN:\S+ ')
+        intents.update([s[3:-1] for s in reg.findall(row['semantic_parse'])])
+        dialogues.append({
+            'dialogue_id': row['index'],
+            'turns': [{
+                'turn_id': 0,
+                'speaker': 'user',
+                'utterance': row['utterance'],
+                'annotations': row['semantic_parse']
+            }]
+        })
+    return dialogues, intents
+
+
+# %% Define Banking-8k dataset
+# %% Test code: load a dataset
+obj = load_atis_dataset()
+obj['train']['meta']['intent2slots']
---
+++ utils/preprocess.py
@@ -0,0 +1,348 @@
+import os
+import re
+import pandas as pd
+import numpy as np
+import json
+
+
+def assemble_output_sequence(utterance: str, slots: dict, intent: str):
+    '''Assemble utterance, slots and intent into a unified output sequences'''
+    output = utterance
+    for name, value in slots.items():
+        output = output.replace(value, f'[SL:{name.upper()} {value}]')
+    return f'[IN:{intent.upper()} {output}]'
+
+
+def _read_one_atis_sample(f):
+    '''Read one sample of ATIS datasets
+
+    :param f: file handle
+    :return: utterance, slots and intent
+    '''
+    sentence = []
+    taggings = []
+    line = f.readline().strip()
+    if line == '':
+        return None
+    while line != '':
+        pair = line.split(' ')
+        sentence.append(pair[0])
+        if len(pair) > 1:
+            taggings.append(pair[1])
+        line = f.readline().strip()
+    sentence, intent = sentence[:-1], sentence[-1]
+
+    slots = {}
+    for i, tag in enumerate(taggings):
+        if tag.startswith('B-'):
+            slots[tag[2:]] = sentence[i]
+        if tag.startswith('I-'):
+            slots[tag[2:]] += ' ' + sentence[i]
+
+    utterance = ' '.join(sentence).strip()
+    return utterance, slots, intent
+
+
+def load_atis_dataset(path='raw_datasets/ATIS'):
+    '''Load ATIS dataset from given dictionary
+
+    :param path: data path, defaults to 'raw_datasets/ATIS'
+    :type path: str, optional
+    :return: dataset df
+    :rtype: pd.DataFrame
+    '''
+    intents = set()
+    datasets = {}
+    for dataset_name in ['dev', 'test', 'train']:
+        datasets[dataset_name] = pd.DataFrame(
+            columns=['utterance', 'output'])
+        with open(os.path.join(path, dataset_name+'.txt'), 'r') as f:
+            while True:
+                sample = _read_one_atis_sample(f)
+                if sample == None:
+                    break
+                utterance, slots, intent = sample
+                output = assemble_output_sequence(utterance, slots, intent)
+                intents.add(intent)
+                datasets[dataset_name] = datasets[dataset_name].append(
+                    {'utterance': utterance, 'output': output}, ignore_index=True)
+
+    return datasets['train'], datasets['dev'], datasets['test'], list(intents)
+
+
+def load_banking_dataset(path='raw_datasets/Banking-8k', train_dev_ratio=[0.8, 0.1]):
+    """Load Banking-8k dataset"""
+    intents = json.load(open(os.path.join(path, 'categories.json')))
+    # Load test set
+    df = pd.read_csv(os.path.join(path, 'test.csv'))
+    test = pd.DataFrame(columns=['utterance', 'output'])
+    for row in df.iloc:
+        output = assemble_output_sequence(row['text'], {}, row['category'])
+        test = test.append({
+            'utterance': row['text'],
+            'output': output
+        }, ignore_index=True)
+
+    # Load train set
+    df = pd.read_csv(os.path.join(path, 'train.csv'))
+    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+    train_size = int(train_dev_ratio[0] * len(df))
+    train = pd.DataFrame(columns=['utterance', 'output'])
+    dev = pd.DataFrame(columns=['utterance', 'output'])
+    for i, row in enumerate(df.iloc):
+        output = assemble_output_sequence(row['text'], {}, row['category'])
+        if i < train_size:
+            train = train.append({
+                'utterance': row['text'],
+                'output': output
+            }, ignore_index=True)
+        else:
+            dev = dev.append({
+                'utterance': row['text'],
+                'output': output
+            }, ignore_index=True)
+
+    return train, dev, test, intents
+
+
+def load_ecdt_dataset(path='raw_datasets/ECDT', train_dev_test_ratio=[0.8, 0.1, 0.1]):
+    obj = json.load(open(os.path.join(path, 'train.json'), 'r'))
+    intents = set()
+    df = pd.DataFrame(columns=['utterance', 'output'])
+    for sample in obj:
+        utterance = sample['text']
+        intent = sample['intent']
+        slots = sample['slots']
+        output = assemble_output_sequence(utterance, slots, intent)
+        df = df.append({'utterance': utterance, 'output': output},
+                       ignore_index=True)
+        intents.add(intent)
+    train_ratio, dev_ratio, _ = train_dev_test_ratio
+    train, dev, test = np.split(df.sample(frac=1, random_state=42),
+                                [int(train_ratio*len(df)), int((train_ratio+dev_ratio)*len(df))])
+    return train, dev, test, list(intents)
+
+
+def load_mix_dataset(path='raw_datasets/MixIntent/MixATIS_clean'):
+    '''Load ATIS dataset from given dictionary'''
+    intents = set()
+    datasets = {}
+    for dataset_name in ['dev', 'test', 'train']:
+        datasets[dataset_name] = pd.DataFrame(
+            columns=['utterance', 'output'])
+        with open(os.path.join(path, dataset_name+'.txt'), 'r') as f:
+            while True:
+                sample = _read_one_atis_sample(f)
+                if sample == None:
+                    break
+                utterance, slots, intent = sample
+                output = assemble_output_sequence(utterance, slots, intent)
+                intents.update(intent.split('#'))
+                datasets[dataset_name] = datasets[dataset_name].append(
+                    {'utterance': utterance, 'output': output}, ignore_index=True)
+
+    return datasets['train'], datasets['dev'], datasets['test'], list(intents)
+
+
+def load_top_dataset(path='raw_datasets/TOPv2'):
+    """Load a TopV2 dataset
+
+    :param path: dataset path, defaults to 'raw_datasets/TOPv2'
+    :type path: str, optional
+    :return: dataset object
+    """
+    datasets = {}
+    domains = ['alarm', 'event', 'messaging', 'music',
+               'navigation', 'reminder', 'timer', 'weather']
+    intents = set()
+    reg = re.compile('IN:\S+ ')
+    for dataset_name in ['train', 'dev', 'test']:
+        datasets[dataset_name] = []
+        if dataset_name == 'train':
+            dataset_name_suffix = '_train.tsv'
+        elif dataset_name == 'dev':
+            dataset_name_suffix = '_eval.tsv'
+        else:
+            dataset_name_suffix = '_test.tsv'
+
+        for domain in domains:
+            df = pd.read_csv(os.path.join(
+                path, domain+dataset_name_suffix), sep='\t')
+            for row in df.iloc:
+                datasets[dataset_name].append({
+                    "utterance": row['utterance'],
+                    "output": row['semantic_parse']
+                })
+                intents.update([s[3:-1]
+                                for s in reg.findall(row['semantic_parse'])])
+
+    train = pd.DataFrame(datasets['train'])
+    dev = pd.DataFrame(datasets['dev'])
+    test = pd.DataFrame(datasets['test'])
+    return train, dev, test, list(intents)
+
+
+def load_hwu64_dataset(path='raw_datasets/HWU64', train_dev_test_ratio=[.8, .1, .1]):
+    intents = set()
+    df = pd.read_csv(os.path.join(path, 'all.txt'), sep=';').reset_index()
+
+    datasets = []
+    reg = re.compile(r'\[[^\[\]]*\]')
+    for row in df.iloc:
+        if type(row['answer_annotation']) != str:
+            continue
+
+        intent = row['intent']
+        slot_pattern = reg.findall(row['answer_annotation'])
+        utterance = row['answer_annotation']
+        output = utterance
+        for pattern in slot_pattern:
+            name, value = pattern[1:-1].split(':')
+            name = name.strip()
+            value = value.strip()
+            utterance = utterance.replace(pattern, value)
+            output = output.replace(name+' ', 'SL:' + name.upper())
+        output = f"[IN:{intent.upper()} {output}]"
+        datasets.append({
+            'utterance': utterance,
+            'output': output
+        })
+        intents.add(intent)
+
+    df = pd.DataFrame(datasets)
+    train_ratio, _, test_ratio = train_dev_test_ratio
+    train, dev, test = np.split(
+        df.sample(frac=1, random_state=42), [int(train_ratio * len(df)), int((1-test_ratio) * len(df))])
+
+    return train, dev, test, list(intents)
+
+
+def _read_one_cais_sample(f_tag, f_in):
+    sentence = []
+    taggings = []
+    line = f_tag.readline().strip()
+    intent = f_in.readline().strip()
+    if line == '':
+        return None
+
+    while line != '':
+        pair = line.split(' ')
+        if len(pair) == 2:
+            word, tag = pair
+        else:
+            word = ' '
+            tag = pair[0]
+        sentence.append(word)
+        taggings.append(tag)
+        line = f_tag.readline().strip()
+
+    slots = {}
+    for i, tag in enumerate(taggings):
+        if tag.startswith('B-'):
+            slots[tag[2:]] = sentence[i]
+        if tag.startswith('I-') or tag.startswith('E-'):
+            slots[tag[2:]] += '' + sentence[i]
+
+    utterance = ''.join(sentence).strip()
+    return utterance, slots, intent
+
+
+def load_cais_dataset(path='raw_datasets/CAIS'):
+    intents = set()
+    datasets = {}
+    for dataset_name in ['valid', 'test', 'train']:
+        datasets[dataset_name] = []
+        f_tag = open(os.path.join(path, dataset_name, 'ch.' + dataset_name))
+        f_in = open(os.path.join(path, dataset_name,
+                                 'ch.' + dataset_name + '.intent'))
+
+        while True:
+            sample = _read_one_cais_sample(f_tag, f_in)
+            if sample == None:
+                break
+            utterance, slots, intent = sample
+            output = assemble_output_sequence(utterance, slots, intent)
+            intents.add(intent)
+            datasets[dataset_name].append({
+                'utterance': utterance,
+                'output': output
+            })
+
+        f_tag.close()
+        f_in.close()
+
+    train = pd.DataFrame(datasets['train'])
+    valid = pd.DataFrame(datasets['valid'])
+    test = pd.DataFrame(datasets['test'])
+
+    return train, valid, test, list(intents)
+
+# train, dev, test, intents = load_atis_dataset()
+# train.to_csv('processed_datasets/ATIS/train.tsv', sep='\t', index=False)
+# dev.to_csv('processed_datasets/ATIS/dev.tsv', sep='\t', index=False)
+# test.to_csv('processed_datasets/ATIS/test.tsv', sep='\t', index=False)
+# with open('processed_datasets/ATIS/intent.txt', 'w') as f:
+#     f.write(' '.join(intents))
+# print('Successfully processed ATIS dataset!')
+
+
+# train, dev, test, intents = load_banking_dataset()
+# train.to_csv('processed_datasets/Banking-8k/train.tsv', sep='\t', index=False)
+# dev.to_csv('processed_datasets/Banking-8k/dev.tsv', sep='\t', index=False)
+# test.to_csv('processed_datasets/Banking-8k/test.tsv', sep='\t', index=False)
+# with open('processed_datasets/Banking-8k/intent.txt', 'w') as f:
+#     f.write(' '.join(intents))
+# print('Successfully processed Banking dataset!')
+
+
+# train, dev, test, intents = load_ecdt_dataset()
+# train.to_csv('processed_datasets/ECDT/train.tsv', sep='\t', index=False)
+# dev.to_csv('processed_datasets/ECDT/dev.tsv', sep='\t', index=False)
+# test.to_csv('processed_datasets/ECDT/test.tsv', sep='\t', index=False)
+# with open('processed_datasets/ECDT/intent.txt', 'w') as f:
+#     f.write(' '.join(intents))
+# print('Successfully processed ECDT dataset!')
+
+
+# train, dev, test, intents = load_mix_dataset()
+# train.to_csv('processed_datasets/MixATIS/train.tsv', sep='\t', index=False)
+# dev.to_csv('processed_datasets/MixATIS/dev.tsv', sep='\t', index=False)
+# test.to_csv('processed_datasets/MixATIS/test.tsv', sep='\t', index=False)
+# with open('processed_datasets/MixATIS/intent.txt', 'w') as f:
+#     f.write(' '.join(intents))
+# print('Successfully processed MixATIS dataset!')
+
+
+# train, dev, test, intents = load_mix_dataset(
+#     path='raw_datasets/MixIntent/MixSNIPS_clean')
+# train.to_csv('processed_datasets/MixSNIPS/train.tsv', sep='\t', index=False)
+# dev.to_csv('processed_datasets/MixSNIPS/dev.tsv', sep='\t', index=False)
+# test.to_csv('processed_datasets/MixSNIPS/test.tsv', sep='\t', index=False)
+# with open('processed_datasets/MixSNIPS/intent.txt', 'w') as f:
+#     f.write(' '.join(intents))
+# print('Successfully processed MixSNIPS dataset!')
+
+
+# train, dev, test, intents = load_top_datset()
+# train.to_csv('processed_datasets/TOP/train.tsv', sep='\t', index=False)
+# dev.to_csv('processed_datasets/TOP/dev.tsv', sep='\t', index=False)
+# test.to_csv('processed_datasets/TOP/test.tsv', sep='\t', index=False)
+# with open('processed_datasets/TOP/intent.txt', 'w') as f:
+#     f.write(' '.join(intents))
+# print('Successfully processed TOP dataset!')
+
+# train, dev, test, intents = load_hwu64_dataset()
+# train.to_csv('processed_datasets/HWU64/train.tsv', sep='\t', index=False)
+# dev.to_csv('processed_datasets/HWU64/dev.tsv', sep='\t', index=False)
+# test.to_csv('processed_datasets/HWU64/test.tsv', sep='\t', index=False)
+# with open('processed_datasets/HWU64/intent.txt', 'w') as f_tag:
+#     f_tag.write(' '.join(intents))
+# print('Successfully processed HWU64 dataset!')
+
+train, dev, test, intents = load_cais_dataset()
+train.to_csv('processed_datasets/CAIS/train.tsv', sep='\t', index=False)
+dev.to_csv('processed_datasets/CAIS/dev.tsv', sep='\t', index=False)
+test.to_csv('processed_datasets/CAIS/test.tsv', sep='\t', index=False)
+with open('processed_datasets/CAIS/intent.txt', 'w') as f_tag:
+    f_tag.write(' '.join(intents))
+print('Successfully processed CAIS dataset!')
